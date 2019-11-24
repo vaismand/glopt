1f480de47a413b49fc992c7f0b65b5928c872cf5
David <omgemjey@gmail.com>
2019-11-24 14:12:40 +0100

    carousel

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,27 +1,17 @@
 $(document).ready(function(){
-  $('.feedback_carousel').slick({
-      centerMode: true,
-      centerPadding: '60px',
-      slidesToShow: 3,
-      arrows: true,
-      variableWidth: true,
-      prevArrow: '<button type="button" class="slick-prev"><img src="../icons/left.png"></button>',
-      nextArrow: '<button type="button" class="slick-next"><img src="../icons/right.png"></button>',
-      responsive: [
-          {
-            breakpoint: 800,
-            settings: {
-              arrows: false,
-              infinite: true,
-              dots: false
-              }
-          },
-          {
-              breakpoint: 450,
-              settings: {
-                  arrows: false,
-              }
-          }
-      ]
-  });
+    $('.carousel_inner').slick({
+        speed: 1200,
+        adaptiveHeight: true,
+        prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.png"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="icons/right.png"></button>',
+        responsive: [
+            {
+                breakpoint: 992,
+                settings: {
+                    dots: true,
+                    arrows: false
+                }
+            }
+        ]
+    });
 });
