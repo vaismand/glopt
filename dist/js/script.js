fd35b6b93a69b741c3317ecb5ae4683874d3fe6c
David <omgemjey@gmail.com>
2019-11-25 15:19:08 +0100

    media

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,7 +1,9 @@
 $(document).ready(function(){
     $('.carousel_inner').slick({
-        speed: 1200,
-        adaptiveHeight: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: true,
+        dots: false,
         prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.png"></button>',
         nextArrow: '<button type="button" class="slick-next"><img src="icons/right.png"></button>',
         responsive: [
--- src/js/script.js
+++ src/js/script.js
@@ -1,15 +1,17 @@
 $(document).ready(function(){
     $('.carousel_inner').slick({
-        speed: 1200,
-        adaptiveHeight: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: true,
+        dots: false,
         prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.png"></button>',
         nextArrow: '<button type="button" class="slick-next"><img src="icons/right.png"></button>',
         responsive: [
             {
                 breakpoint: 992,
                 settings: {
-                    dots: true,
-                    arrows: false
+                    dots: false,
+                    arrows: true
                 }
             }
         ]
