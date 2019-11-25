eb6584f55d80ecd07f948d4f489addff4836f980
David <omgemjey@gmail.com>
2019-11-25 20:11:59 +0100

    media mobile

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,3 +1,21 @@
+window.addEventListener('DOMContentLoaded', () => {
+    const menu = document.querySelector('.menu'),
+    menuItem = document.querySelectorAll('.menu_item'),
+    hamburger = document.querySelector('.hamburger');
+
+    hamburger.addEventListener('click', () => {
+        hamburger.classList.toggle('hamburger_active');
+        menu.classList.toggle('menu_active');
+    });
+
+    menuItem.forEach(item => {
+        item.addEventListener('click', () => {
+            hamburger.classList.toggle('hamburger_active');
+            menu.classList.toggle('menu_active');
+        });
+    });
+});
+
 $(document).ready(function(){
     $('.carousel_inner').slick({
         slidesToShow: 1,
@@ -10,8 +28,8 @@ $(document).ready(function(){
             {
                 breakpoint: 992,
                 settings: {
-                    dots: true,
-                    arrows: false
+                    dots: false,
+                    arrows: true
                 }
             }
         ]
