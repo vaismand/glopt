a1ee3791b94df700570050e2baefcc3945f90b4c
David <omgemjey@gmail.com>
2019-11-26 00:09:24 +0100

    modal

--- dist/js/script.js
+++ dist/js/script.js
@@ -35,3 +35,10 @@ $(document).ready(function(){
         ]
     });
 });
+
+$('[data-modal=consultation]').on('click', function() {
+    $('.overlay, #consultation').fadeIn('slow');
+});
+$('.modal__close').on('click', function() {
+    $('.overlay, #consultation, #thanks, #order').fadeOut('slow');
+});
